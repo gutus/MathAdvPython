e3cc9378dd7d9ae5be7f76cf9192bc80b7d1967a
gutus <gutusnirwanto@gmail.com>
2021-07-31 11:22:22 +0700

    master

---
+++ 00_function_quadaratic.py
@@ -0,0 +1,17 @@
+# Python for Math
+
+import math
+
+
+def f(x):
+    return math.sqrt(x+3)-x+1
+
+
+# iterate f(x) when f(0), f(1), f(math.sqrt(2)), f(math.sqrt(2)-1)
+for x in [0, 1, math.sqrt(2), math.sqrt(2)-1]:
+    print("f({: .3f})={: .3f}".format(x, f(x)))
+
+
+print("\n\n\n\t Dengan Iterasi range f(x) dengan range x (1-5), kita bisa menggunakan lebih banyak sample point")
+for x in range(0, 5):
+    print("f({: .3f})={: .3f}".format(x, f(x)))
