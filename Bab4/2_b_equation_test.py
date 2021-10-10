533c8b3f82f89c861f29bf3d4a3b2505f79764a2
Gutus Nirwanto <gutusnirwanto@gmail.com>
2021-10-10 11:35:13 +0700

    master

---
+++ Bab4/2_b_equation_test.py
@@ -0,0 +1,23 @@
+# Equation Function
+
+from fractions import Fraction  # untuk mengkonversi fraction 1/2 menjadi float
+
+
+def equation(a, b, c, d):  # Penyelesaian persamaan ax + b = cx + d
+    return (d-b)/(a-c)
+
+
+a = float(Fraction(input("Masukkan nilai a dari persamaan ax + b = cx + d >>> ")))
+b = float(Fraction(input("Masukkan nilai b dari persamaan ax + b = cx + d >>> ")))
+c = float(Fraction(input("Masukkan nilai c dari persamaan ax + b = cx + d >>> ")))
+d = float(Fraction(input("Masukkan nilai d dari persamaan ax + b = cx + d >>> ")))
+print(
+    f"Nilai x  {equation(a, b, c, d)}")
+x = equation(a, b, c, d)
+print("Persamaan  ax + b = cx + d >>> ")
+print(
+    f"\nPersamaan {Fraction(a)}*{x} + {Fraction(b)}= {Fraction(c)}*{x} + {Fraction(d)} ")
+print(f"\nSehingga >>> {a*x} + {b}= {c*x} + {d} ")
+
+# Periksa dengan memasukkan nilai equation (1/2,2/3,1/5,7/8) nilai x adalah 0.6944444444444446
+# Rieuuuut.... :D
